35e7dd4acded1eb1a669680d23cd3f805ce83bcc
Pasindu Udawatta <pasindu.secon@gmail.com>
2023-05-28 14:25:29 +0530

    Configured OpenCV

--- app/src/main/java/com/pasindu_ud/light_chat/ReceivingActivity.java
+++ app/src/main/java/com/pasindu_ud/light_chat/ReceivingActivity.java
@@ -1,13 +1,22 @@
 package com.pasindu_ud.light_chat;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.opencv.android.OpenCVLoader;
 
 public class ReceivingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.receiving_activity);
+
+        if (OpenCVLoader.initDebug()) {
+            Log.d("LOADED_OPENCV", "Successful");
+        } else {
+            Log.d("LOADED_OPENCV", "Unsuccessful");
+        }
     }
 }
